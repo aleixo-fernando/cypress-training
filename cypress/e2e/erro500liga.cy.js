f5c97f74ddeb2df4b95819cb117bb6555f9b1e11
Fernando Aleixo <fernandoaleixo.contact@gmail.com>
2024-11-03 00:03:06 -0300

    Pequena atualização

--- cypress/e2e/erro500liga.cy.js
+++ cypress/e2e/erro500liga.cy.js
@@ -37,7 +37,7 @@ Cypress.Commands.add('monitor500Errors', () => {
   
     it('should not produce any 500 errors', () => {
       var ednumb=641
-      while(ednumb>630){
+      while(ednumb>1){
       cy.visit(`https://www.ligapokemon.com.br/?view=cards/search&card=edid=${ednumb}%20ed=SVM`);
       // Replace with the page you want to test
       // Add your test steps here
